de08b8e1bfda06c40879d16ca264bfc2f54b0664
deepak821 <pandeydeepak821@gmail.com>
2021-06-08 14:22:42 +0530

    updated work

--- Backend/index.js
+++ Backend/index.js
@@ -3,23 +3,24 @@ const mongoose = require("mongoose");
 const path = require("path");
 const hbs =require('hbs')
 const app = express();
-const Port = 8000;
+const Port = 8000;    
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use("/images")
-const staticpath = path.join(__dirname, "../public")
+app.use(express.urlencoded({extended:false})); 
+const staticpath = path.join(__dirname, "./public")   
 const templatespath = path.join(__dirname, "./templates/views")
 const partialspath = path.join(__dirname, "./templates/partials")
 
 app.use(express.static(staticpath))
 app.set("view engine", "hbs")
-app.set("views", templatespath)
+app.set("views", templatespath) 
 hbs.registerPartials(partialspath)
 
 app.get("/", (req, res) => {
     res.render("index") 
 })
-
+app.get("/AddLink", (req, res) => {
+    res.render("AddLink")   
+})
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/GetyoLink', {useNewUrlParser: true, useCreateIndex: true,
 useUnifiedTopology: true, useFindAndModify: false});
 
